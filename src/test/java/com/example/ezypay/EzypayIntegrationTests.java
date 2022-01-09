0d703ad53978542ee1917abda8bce23acd802a8d
Aaron Liew <aaron_liew1818@hotmail.com>
2022-01-09 17:48:36 +0800

    [FEAT] Added integration test

---
+++ src/test/java/com/example/ezypay/EzypayIntegrationTests.java
@@ -0,0 +1,13 @@
+package com.example.ezypay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzypayIntegrationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
